ba6746b50ab6b5ab373be4edca013c8ea9db6a37
vInTaGe-CoDe <tophictango@outlook.com>
2023-04-11 08:45:58 -0700

    Instance class or inherited class

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-
-"""Checks if object is an instance of a class or from an inherited class"""
+"""checks if object is an instance of a class
+or an inherited class
+"""
 
 
 def is_kind_of_class(obj, a_class):
-    
-"""Returns true if object is an instance of a class or a class from which the class inherits"""
+    """Returns true if object is an instance of a class
+    or a class from which the class inherits
+    """
     return (isinstance(obj, a_class))
