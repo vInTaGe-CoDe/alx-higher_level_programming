8627122261c38c8f86f92801205ff022d59920bf
vInTaGe-CoDe <tophictango@outlook.com>
2023-04-11 10:17:04 -0700

    0-lookup.py changes

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""
-    This module returns the list of available attributes and methods of an object
-"""
+
+"""This module returns the list of available attributes and methods of an object"""
 
 
 def lookup(obj):
 
-    """Returns a list of available attributes and methods of an object"""
+    """This function lists all available attributes and methods of an object"""
     return dir(obj)
